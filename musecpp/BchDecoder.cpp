718cf539cab2b08fc2898b570601879059f66907
Staffan Ulfberg <staffan@ulfberg.se>
2023-06-21 21:40:24 +0200

    Split BchDecoder into .h and .cpp

--- musecpp/BchDecoder.cpp
+++ musecpp/BchDecoder.cpp
@@ -1,3 +1,50 @@
 //
 // Created by Staffan Ulfberg on 6/16/23.
 //
+
+#include "BchDecoder.h"
+
+BchDecoder::BchDecoder(int n, int k, int generator)
+: m_n(n),
+  m_k(k),
+  m_generator(generator) {
+    // Notice we only compute the powers up to n - 1.  This means that in the case of
+    // multiple errors the log could fail, but we cannot correct in that case anyway.
+    // If we make the log table complete we need to check if the log is in range for shortened codes.
+    int alpha_pow = 1;
+    for (int i = 0; i < n; i++) {
+        m_alpha_powers.push_back(alpha_pow);
+        m_logs[alpha_pow] = i;
+        if ((alpha_pow & (1 << (n - k - 1 - 1))) != 0)
+            alpha_pow = (alpha_pow << 1) ^ generator;
+        else
+            alpha_pow = alpha_pow << 1;
+    }
+}
+
+bool BchDecoder::decode(int bits[]) {
+    int s0 = 0;
+    int s1 = 0;
+    for (int i = 0; i < m_n; i++) {
+        s0 ^= bits[m_n - i - 1];
+        s1 ^= bits[m_n - i - 1] * m_alpha_powers[i];
+    }
+    if (s0 == 0 && s1 == 0) {
+        m_statistics["input ok"]++;
+        return true;
+    } else if (s0 != 0) {
+        auto it = m_logs.find(s1);
+        if (it == m_logs.end()) {
+            m_statistics["cannot correct bad syndrome"]++;
+            return false;
+        } else {
+            int logS1 = it->second;
+            bits[m_n - logS1 - 1] ^= 1;
+            m_statistics["corrected 1 error"]++;
+            return true;
+        }
+    } else {
+        m_statistics["cannot correct two errors"]++;
+        return false;
+    }
+}
--- musecpp/BchDecoder.h
+++ musecpp/BchDecoder.h
@@ -24,59 +24,18 @@
  */
 class BchDecoder {
 public:
-    BchDecoder(int n, int k, int generator):
-    m_n(n),
-    m_k(k),
-    m_generator(generator) {
-        // Notice we only compute the powers up to n - 1.  This means that in the case of
-        // multiple errors the log could fail, but we cannot correct in that case anyway.
-        // If we make the log table complete we need to check if the log is in range for shortened codes.
-        int alpha_pow = 1;
-        for (int i = 0; i < n; i++) {
-            m_alpha_powers.push_back(alpha_pow);
-            m_logs[alpha_pow] = i;
-            if ((alpha_pow & (1 << (n - k - 1 - 1))) != 0)
-                alpha_pow = (alpha_pow << 1) ^ generator;
-            else
-                alpha_pow = alpha_pow << 1;
-        }
-    }
+    BchDecoder(int n, int k, int generator);
+
+    // bits are left to right with msb first -- we therefore index bit i (0 based) as bits(n - i - 1)
+    bool decode(int bits[]);
 
     void resetStatistics() {
         m_statistics.clear();
     }
 
     void printStatistics() {
-        for (auto el: m_statistics)
+        for (const auto &el: m_statistics)
             std::cout << el.first << ": " << el.second << std::endl;
-    }
-
-    // bits are left to right with msb first -- we therefore index bit i (0 based) as bits(n - i - 1)
-    bool decode(int bits[]) {
-        int s0 = 0;
-        int s1 = 0;
-        for (int i = 0; i < m_n; i++) {
-            s0 ^= bits[m_n - i - 1];
-            s1 ^= bits[m_n - i - 1] * m_alpha_powers[i];
-        }
-        if (s0 == 0 && s1 == 0) {
-            m_statistics["input ok"]++;
-            return true;
-        } else if (s0 != 0) {
-            auto it = m_logs.find(s1);
-            if (it == m_logs.end()) {
-                m_statistics["cannot correct bad syndrome"]++;
-                return false;
-            } else {
-                int logS1 = it->second;
-                bits[m_n - logS1 - 1] ^= 1;
-                m_statistics["corrected 1 error"]++;
-                return true;
-            }
-        } else {
-            m_statistics["cannot correct two errors"]++;
-            return false;
-        }
     }
 
 private:
