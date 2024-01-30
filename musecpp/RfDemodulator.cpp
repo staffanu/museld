2c374143503197615d3dccc42d59227f13510c4e
Staffan Ulfberg <staffan@ulfberg.se>
2024-01-30 11:42:06 +0100

    Code cleanup

--- musecpp/RfDemodulator.cpp
+++ musecpp/RfDemodulator.cpp
@@ -4,147 +4,11 @@
 
 #include <cmath>
 #include <thread>
-#include <complex>
 #include <vector>
 #include <iostream>
-#include <array>
 #include "RfDemodulator.h"
 
 using namespace std;
-
-// The filters assume an input sampling rate of 62.5 MHz.  In order to allow that to be set in runtime
-// we could link against the gnuradio libraries.
-constexpr int decimation_rate = 2;
-constexpr int sample_block_size = 4096;
-
-// FIR passband filter to filter out noise over 22.5 MHz and also the pilot signal and EFM.
-// The pilot signal isn't completely supressed but that doesn't seem to matter.
-// Computed using gnuradio:
-// firdes.complex_band_pass(1.0, 62.5e6, 2.7e6, 22.3e6, 2e6, window.WIN_RECTANGULAR)
-const vector<complex<float>> bandpass_filter_def = {
-        complex<float>(0.0065876576118171215, 0.020274756476283073),
-        complex<float>(-0.004714661277830601, 0.00342539488337934),
-        complex<float>(0.01447536051273346, 0.010516997426748276),
-        complex<float>(-0.00879573542624712, 0.02707030437886715),
-        complex<float>(-0.013138349168002605, -3.2994755372328655e-08),
-        complex<float>(0.005763731896877289, 0.017739124596118927),
-        complex<float>(-0.03205157443881035, 0.02328665927052498),
-        complex<float>(-0.021089427173137665, -0.015322495251893997),
-        complex<float>(-0.005935038439929485, 0.018265916034579277),
-        complex<float>(-0.061971265822649, -2.5511641865705315e-07),
-        complex<float>(-0.017557775601744652, -0.05403804033994675),
-        complex<float>(-0.015810636803507805, 0.01148699875921011),
-        complex<float>(-0.11814974993467331, -0.0858415812253952),
-        complex<float>(0.08165450394153595, -0.2513030767440796),
-        complex<float>(0.3123723268508911, 1.3405565368884709e-06),
-        complex<float>(0.08165234327316284, 0.2513037919998169),
-        complex<float>(-0.1181504875421524, 0.08584056794643402),
-        complex<float>(-0.015810538083314896, -0.011487134732306004),
-        complex<float>(-0.01755823940038681, 0.05403788760304451),
-        complex<float>(-0.061971265822649, -2.76787233133291e-07),
-        complex<float>(-0.005934881512075663, -0.018265968188643456),
-        complex<float>(-0.02108955755829811, 0.015322314575314522),
-        complex<float>(-0.03205139935016632, -0.023286905139684677),
-        complex<float>(0.005763866938650608, -0.017739081755280495),
-        complex<float>(-0.013138349168002605, -6.724289391968341e-08),
-        complex<float>(-0.008795528672635555, -0.02707037143409252),
-        complex<float>(0.014475440606474876, -0.010516887530684471),
-        complex<float>(-0.0047146352007985115, -0.0034254309721291065),
-        complex<float>(0.006587812211364508, -0.020274706184864044),
-        complex<float>(0, 0), // make size multiple of 8
-        complex<float>(0, 0), // make size multiple of 8
-        complex<float>(0, 0), // make size multiple of 8
-};
-constexpr int bandpass_filter_size = 32;
-
-// FIR lowpass filter for the demodulated signal
-// Computed using gnuradio: firdes.low_pass(1.0, 62.5e6, 9.8e6, 2e6, window.WIN_RECTANGULAR)
-vector<float> lowpass_filter = {
-        0.021318137645721436,
-        0.005827637854963541,
-        -0.01789254881441593,
-        -0.028463421389460564,
-        -0.013138349168002605,
-        0.01865200139582157,
-        0.03961782529950142,
-        0.02606804110109806,
-        -0.01920594647526741,
-        -0.061971265822649,
-        -0.0568188801407814,
-        0.019542962312698364,
-        0.14604157209396362,
-        0.26423606276512146,
-        0.3123723268508911,
-        0.26423606276512146,
-        0.14604157209396362,
-        0.019542962312698364,
-        -0.0568188801407814,
-        -0.061971265822649,
-        -0.01920594647526741,
-        0.02606804110109806,
-        0.03961782529950142,
-        0.01865200139582157,
-        -0.013138349168002605,
-        -0.028463421389460564,
-        -0.01789254881441593,
-        0.005827637854963541,
-        0.021318137645721436,
-        0,
-        0,
-        0,
-};
-constexpr int lowpass_filter_size = 32;
-
-// Root raised cosine filter for pulse shaping
-// Notice that the root raised cosine filter works on half the sample rate, i.e., 31.25 MHz
-// Computed in gnuradio using RRC filter taps with Sample rate 31.25 MHz, Symbol rate 16.2 MHz, Excess BW 0.1.
-vector<float> rrc_filter = vector<float>{
-    0.048089, 0.028415, -0.092251, -0.029997, 0.296034, 0.499418, 0.296034, -0.029997, -0.092251, 0.028415, 0.048089, 0, 0, 0, 0, 0
-};
-constexpr int rrc_filter_size = 16;
-
-#define AVX_FLOATS 8
-
-#ifdef __AVX__
-#include <immintrin.h>
-#include <numeric>
-
-void firFilter(
-        const float *input,   // input signal of length at least output_size + filter_length - 1
-        size_t output_size,   // number of output values to compute
-        float *filter,        // reversed filter coefficients
-        size_t filter_size,
-        float *output) {
-    array<float, AVX_FLOATS> tmp_store{};
-
-    for (int i = 0; i < output_size; i++) {
-        __m256 out_chunk = _mm256_setzero_ps();
-        for (int j = 0; j < filter_size; j += AVX_FLOATS) {
-            __m256 input_chunk = _mm256_loadu_ps(input + i + j);
-            __m256 filter_chunk = _mm256_loadu_ps(filter + j);
-            out_chunk = _mm256_add_ps(out_chunk, _mm256_mul_ps(input_chunk, filter_chunk));
-        }
-        _mm256_storeu_ps(tmp_store.data(), out_chunk);
-        output[i] = std::accumulate(tmp_store.begin(), tmp_store.end(), 0.f);
-    }
-}
-
-#else
-#warning "AVX not detected"
-void firFilter(
-        const float *input,   // input signal of length output_length + filter_length - 1
-        size_t output_length, // usable input (not including the filter_length-1 extra values)
-        const float *filter,  // reversed filter coefficients
-        size_t filter_length,
-        float *output) {
-    for (auto i = 0; i < output_length; i++) {
-        output[i] = 0;
-        for (auto j = 0; j < filter_length; j++) {
-            output[i] += input[i + j] * filter[j];
-        }
-    }
-}
-#endif
 
 bool RfDemodulator::initialize() {
     m_input_fd = open(m_filename.c_str(), O_NONBLOCK);
@@ -171,80 +35,68 @@ void RfDemodulator::demodulate() {
     if (m_out_file_fd == -1)
         throw runtime_error("Unable to open output file");
 
-    assert(sample_block_size % decimation_rate == 0);
+    assert(c_sample_block_size % c_decimation_rate == 0);
+    assert(c_bandpass_filter_size % c_AVX_floats_per_chunk == 0);
+    assert(c_lowpass_filter_size % c_AVX_floats_per_chunk == 0);
+    assert(c_rrc_filter_size % c_AVX_floats_per_chunk == 0);
 
-    assert(bandpass_filter_size % AVX_FLOATS == 0);
-    assert(bandpass_filter_size == bandpass_filter_def.size()); // TODO: figure out a way to calculate the size in compile time
-    vector<float> bandpass_filter_re;
-    vector<float> bandpass_filter_im;
-    transform(bandpass_filter_def.rbegin(), bandpass_filter_def.rend(), back_inserter(bandpass_filter_re), [](complex<float> c) -> float { return c.real(); });
-    transform(bandpass_filter_def.rbegin(), bandpass_filter_def.rend(), back_inserter(bandpass_filter_im), [](complex<float> c) -> float { return c.imag(); });
-
-    constexpr int input_buffer_size = sample_block_size + bandpass_filter_size - 1;
-
-    float input_buffer[input_buffer_size];
-    m_tmp_input_buffer = (int16_t *)malloc(input_buffer_size * sizeof(int16_t)); // FIXME
-
-    float analytic_buffer_re[sample_block_size];
-    float analytic_buffer_im[sample_block_size];
-
-    assert(lowpass_filter_size % AVX_FLOATS == 0);
-    assert(lowpass_filter_size == lowpass_filter.size()); // TODO: figure out a way to calculate the size in compile time
-
-    constexpr int lowpass_in_buffer_size = sample_block_size + lowpass_filter_size - 1;
-    float lowpass_in_buffer[lowpass_in_buffer_size];
-    float lowpass_out_buffer[sample_block_size];
-
-    assert(rrc_filter_size % AVX_FLOATS == 0);
-    assert(rrc_filter_size == rrc_filter_size.size()); // TODO: figure out a way to calculate the size in compile time
-
-    constexpr int rrc_in_buffer_size = sample_block_size / decimation_rate + rrc_filter_size - 1;
-    float rrc_in_buffer[rrc_in_buffer_size];
-    float rrc_out_buffer[sample_block_size / decimation_rate];
-
-    constexpr int output_buffer_size = sample_block_size / decimation_rate;
-    int16_t output_buffer[output_buffer_size];
+    float input_buffer[c_input_buffer_size];
+    float analytic_buffer_re[c_sample_block_size];
+    float analytic_buffer_im[c_sample_block_size];
+    float lowpass_in_buffer[c_lowpass_in_buffer_size];
+    float lowpass_out_buffer[c_sample_block_size];
+    float rrc_in_buffer[c_rrc_in_buffer_size];
+    float rrc_out_buffer[c_sample_block_size / c_decimation_rate];
+    int16_t output_buffer[c_output_buffer_size];
 
     float prev_angle;
     auto t0 = chrono::high_resolution_clock::now();
 
-    if (!readFloats(input_buffer, bandpass_filter_size - 1))
+    if (!readFloats(input_buffer, c_bandpass_filter_size - 1))
         throw runtime_error("input file too short");
-    long sample_counter = bandpass_filter_size - 1;
+    long sample_counter = c_bandpass_filter_size - 1;
 
-    while (readFloats(input_buffer + bandpass_filter_size - 1, sample_block_size) && sample_counter < 62500000 * 10) {
-        sample_counter += sample_block_size;
+    while (readFloats(input_buffer + c_bandpass_filter_size - 1, c_sample_block_size) && sample_counter < 62500000 * 10) {
+        sample_counter += c_sample_block_size;
 
-        firFilter(input_buffer, sample_block_size, bandpass_filter_re.data(), bandpass_filter_size, analytic_buffer_re);
-        firFilter(input_buffer, sample_block_size, bandpass_filter_im.data(), bandpass_filter_size, analytic_buffer_im);
+        // First run the input signal through the bandpass filter that also converts the signal into an analytic signal
+        firFilter(input_buffer, c_sample_block_size, c_bandpass_filter.first, analytic_buffer_re);
+        firFilter(input_buffer, c_sample_block_size, c_bandpass_filter.second, analytic_buffer_im);
 
-        for (int index = 0; index < sample_block_size; index++) {
+        // Demodulate the analytic signal
+        for (int index = 0; index < c_sample_block_size; index++) {
             float angle = atan2(analytic_buffer_im[index], analytic_buffer_re[index]);
             float phase_diff = angle > prev_angle ? angle - prev_angle : 2 * (float)M_PI + angle - prev_angle;
             prev_angle = angle;
             float fm_out = (phase_diff * (62.5e6f / (2.f * (float)M_PI * 12.5e6f)) - 1.f) * (12.5e6f / 1.9e6f);
-            lowpass_in_buffer[index + lowpass_filter_size - 1] = fm_out;
+            lowpass_in_buffer[index + c_lowpass_filter_size - 1] = fm_out;
         }
 
-        firFilter(lowpass_in_buffer, sample_block_size, lowpass_filter.data(), lowpass_filter_size, lowpass_out_buffer);
+        // Lowpass filter the demodulated signal
+        firFilter(lowpass_in_buffer, c_sample_block_size, c_lowpass_filter, lowpass_out_buffer);
 
-        for (int index = 0, rcc_index = rrc_filter_size - 1; index < sample_block_size; index += decimation_rate, rcc_index++) {
+        // Down-sample the signal
+        for (int index = 0, rcc_index = c_rrc_filter_size - 1; index < c_sample_block_size; index += c_decimation_rate, rcc_index++) {
             rrc_in_buffer[rcc_index] = lowpass_out_buffer[index];
         }
 
-        firFilter(rrc_in_buffer, sample_block_size / 2, rrc_filter.data(), rrc_filter_size, rrc_out_buffer);
+        // Run the down-sampled signal through the root raised cosine pulse-shaping filter
+        firFilter(rrc_in_buffer, c_sample_block_size / 2, c_rrc_filter, rrc_out_buffer);
 
-        for (int index = 0; index < output_buffer_size; index++) {
+        // Convert the output to ints and scale -- this step could be avoided since the decoder starts with converting
+        // back to floats
+        for (int index = 0; index < c_output_buffer_size; index++) {
             auto value = (int16_t)clamp(rrc_out_buffer[index] * 15000.0, -32768.0, 32767.0);
             output_buffer[index] = value;
         }
 
-        if (write(m_out_file_fd, output_buffer, output_buffer_size * sizeof(int16_t)) != output_buffer_size * sizeof(int16_t))
+        if (write(m_out_file_fd, output_buffer, c_output_buffer_size * sizeof(int16_t)) != c_output_buffer_size * sizeof(int16_t))
             throw runtime_error("write failed");
 
-        memcpy(input_buffer, input_buffer + input_buffer_size - bandpass_filter_size + 1, (bandpass_filter_size - 1) * sizeof(float));
-        memcpy(lowpass_in_buffer, lowpass_in_buffer + lowpass_in_buffer_size - lowpass_filter_size + 1, (lowpass_filter_size - 1) * sizeof(float));
-        memcpy(rrc_in_buffer, rrc_in_buffer + rrc_in_buffer_size - rrc_filter_size + 1, (rrc_filter_size - 1) * sizeof(float));
+        // Copy the end of the filter inputs to the start of the corresponding buffers
+        memcpy(input_buffer, input_buffer + c_input_buffer_size - c_bandpass_filter_size + 1, (c_bandpass_filter_size - 1) * sizeof(float));
+        memcpy(lowpass_in_buffer, lowpass_in_buffer + c_lowpass_in_buffer_size - c_lowpass_filter_size + 1, (c_lowpass_filter_size - 1) * sizeof(float));
+        memcpy(rrc_in_buffer, rrc_in_buffer + c_rrc_in_buffer_size - c_rrc_filter_size + 1, (c_rrc_filter_size - 1) * sizeof(float));
     }
 
     auto t1 = chrono::high_resolution_clock::now();
@@ -253,6 +105,7 @@ void RfDemodulator::demodulate() {
 }
 
 bool RfDemodulator::readFloats(float *out, size_t n) {
+    int16_t m_tmp_input_buffer[c_input_buffer_size];
     int filled_bytes = 0;
     do {
         ssize_t read_count = read(m_input_fd,
@@ -274,3 +127,42 @@ bool RfDemodulator::readFloats(float *out, size_t n) {
 
     return true;
 }
+
+#ifdef __AVX__
+#include <immintrin.h>
+#include <numeric>
+
+template<size_t filter_size> void RfDemodulator::firFilter(
+        const float *input,   // input signal of length at least output_size + filter_length - 1
+        size_t output_size,   // number of output values to compute
+        const array<float, filter_size> &filter,  // reversed filter coefficients, needs to be aligned at 32 byte multiple (c_AVX_floats_per_chunk * sizeof(float))
+        float *output) {
+    array<float, c_AVX_floats_per_chunk> tmp_store{};
+
+    for (int i = 0; i < output_size; i++) {
+        __m256 out_chunk = _mm256_setzero_ps();
+        for (int j = 0; j < filter_size; j += c_AVX_floats_per_chunk) {
+            __m256 input_chunk = _mm256_loadu_ps(input + i + j);
+            __m256 filter_chunk = _mm256_load_ps(filter.data() + j); // notice aligned load
+            out_chunk = _mm256_add_ps(out_chunk, _mm256_mul_ps(input_chunk, filter_chunk));
+        }
+        _mm256_storeu_ps(tmp_store.data(), out_chunk);
+        output[i] = std::accumulate(tmp_store.begin(), tmp_store.end(), 0.f);
+    }
+}
+
+#else
+#warning "AVX not detected"
+template<size_t filter_size> void RfDemodulator::firFilter(
+        const float *input,   // input signal of length output_length + filter_length - 1
+        size_t output_size, // usable input (not including the filter_length-1 extra values)
+        const array<float, filter_size> &filter,  // reversed filter coefficients
+        float *output) {
+    for (auto i = 0; i < output_size; i++) {
+        output[i] = 0;
+        for (auto j = 0; j < filter_size; j++) {
+            output[i] += input[i + j] * filter[j];
+        }
+    }
+}
+#endif
--- musecpp/RfDemodulator.h
+++ musecpp/RfDemodulator.h
@@ -10,7 +10,113 @@
 #include <fstream>
 #include <fmt/format.h>
 #include <cassert>
+#include <complex>
+#include <array>
+#include <algorithm>
 #include "Logger.h"
+
+namespace RfDemodulatorHelper {
+    // FIR passband filter to filter out noise over 22.5 MHz and also the pilot signal and EFM.
+    // The pilot signal isn't completely supressed but that doesn't seem to matter.
+    // Computed using gnuradio:
+    // firdes.complex_band_pass(1.0, 62.5e6, 2.7e6, 22.3e6, 2e6, window.WIN_RECTANGULAR)
+    static constexpr std::pair<std::array<float, 32>, std::array<float, 32>> initializeBandpassFilter() {
+        std::array<std::complex<float>, 32> bandpass_filter_def = {
+                std::complex<float>(0.0065876576118171215, 0.020274756476283073),
+                std::complex<float>(-0.004714661277830601, 0.00342539488337934),
+                std::complex<float>(0.01447536051273346, 0.010516997426748276),
+                std::complex<float>(-0.00879573542624712, 0.02707030437886715),
+                std::complex<float>(-0.013138349168002605, -3.2994755372328655e-08),
+                std::complex<float>(0.005763731896877289, 0.017739124596118927),
+                std::complex<float>(-0.03205157443881035, 0.02328665927052498),
+                std::complex<float>(-0.021089427173137665, -0.015322495251893997),
+                std::complex<float>(-0.005935038439929485, 0.018265916034579277),
+                std::complex<float>(-0.061971265822649, -2.5511641865705315e-07),
+                std::complex<float>(-0.017557775601744652, -0.05403804033994675),
+                std::complex<float>(-0.015810636803507805, 0.01148699875921011),
+                std::complex<float>(-0.11814974993467331, -0.0858415812253952),
+                std::complex<float>(0.08165450394153595, -0.2513030767440796),
+                std::complex<float>(0.3123723268508911, 1.3405565368884709e-06),
+                std::complex<float>(0.08165234327316284, 0.2513037919998169),
+                std::complex<float>(-0.1181504875421524, 0.08584056794643402),
+                std::complex<float>(-0.015810538083314896, -0.011487134732306004),
+                std::complex<float>(-0.01755823940038681, 0.05403788760304451),
+                std::complex<float>(-0.061971265822649, -2.76787233133291e-07),
+                std::complex<float>(-0.005934881512075663, -0.018265968188643456),
+                std::complex<float>(-0.02108955755829811, 0.015322314575314522),
+                std::complex<float>(-0.03205139935016632, -0.023286905139684677),
+                std::complex<float>(0.005763866938650608, -0.017739081755280495),
+                std::complex<float>(-0.013138349168002605, -6.724289391968341e-08),
+                std::complex<float>(-0.008795528672635555, -0.02707037143409252),
+                std::complex<float>(0.014475440606474876, -0.010516887530684471),
+                std::complex<float>(-0.0047146352007985115, -0.0034254309721291065),
+                std::complex<float>(0.006587812211364508, -0.020274706184864044),
+                std::complex<float>(0, 0), // padding to make size multiple of 8
+                std::complex<float>(0, 0),
+                std::complex<float>(0, 0),
+        };
+
+        std::array<float, 32> bandpass_filter_re{};
+        std::array<float, 32> bandpass_filter_im{};
+        for (int i = 0; i < bandpass_filter_def.size(); i++) {
+            bandpass_filter_re[i] = bandpass_filter_def[bandpass_filter_def.size() - i - 1].real();
+            bandpass_filter_im[i] = bandpass_filter_def[bandpass_filter_def.size() - i - 1].imag();
+        }
+
+        return std::make_pair(bandpass_filter_re, bandpass_filter_im);
+    }
+
+    // FIR lowpass filter for the demodulated signal
+    // Computed using gnuradio: firdes.low_pass(1.0, 62.5e6, 9.8e6, 2e6, window.WIN_RECTANGULAR)
+    static constexpr std::array<float, 32> initializeLowpassFilter() {
+        return std::array<float, 32>{
+                0.021318137645721436,
+                0.005827637854963541,
+                -0.01789254881441593,
+                -0.028463421389460564,
+                -0.013138349168002605,
+                0.01865200139582157,
+                0.03961782529950142,
+                0.02606804110109806,
+                -0.01920594647526741,
+                -0.061971265822649,
+                -0.0568188801407814,
+                0.019542962312698364,
+                0.14604157209396362,
+                0.26423606276512146,
+                0.3123723268508911,
+                0.26423606276512146,
+                0.14604157209396362,
+                0.019542962312698364,
+                -0.0568188801407814,
+                -0.061971265822649,
+                -0.01920594647526741,
+                0.02606804110109806,
+                0.03961782529950142,
+                0.01865200139582157,
+                -0.013138349168002605,
+                -0.028463421389460564,
+                -0.01789254881441593,
+                0.005827637854963541,
+                0.021318137645721436,
+                0, // padding to make size multiple of 8
+                0,
+                0,
+        };
+    }
+
+    // Root raised cosine filter for pulse shaping
+    // Notice that the root raised cosine filter works on half the sample rate, i.e., 31.25 MHz
+    // Computed in gnuradio using RRC filter taps with Sample rate 31.25 MHz, Symbol rate 16.2 MHz, Excess BW 0.1.
+    static constexpr std::array<float, 16> initializeRrcFilter() {
+        return std::array<float, 16>{
+                0.048089, 0.028415, -0.092251, -0.029997, 0.296034, 0.499418, 0.296034, -0.029997, -0.092251, 0.028415,
+                0.048089, 0, 0, 0, 0, 0 // padding to make size multiple of 8
+        };
+    }
+}
+
+using namespace RfDemodulatorHelper;
 
 class RfDemodulator {
 public:
@@ -18,9 +124,7 @@ public:
     : m_log(log),
     m_filename(filename),
     m_input_fd(-1),
-    m_input_is_fifo(false),
-    m_tmp_input_buffer{}
-    {
+    m_input_is_fifo(false) {
     }
 
     bool initialize();
@@ -28,14 +132,42 @@ public:
     void cleanup();
 
 private:
+    // The filters assume an input sampling rate of 62.5 MHz.  In order to allow that to be set in runtime
+    // we could link against the gnuradio libraries.
+    static constexpr int c_decimation_rate = 2;
+    static constexpr int c_sample_block_size = 4096;
+
+    static constexpr int c_AVX_floats_per_chunk = 8;
+
+    // The AVX code assumed that the filters fulfill the alignment requirements for __m256
+    static constexpr auto __m256_alignment = c_AVX_floats_per_chunk * alignof(float);
+
+    alignas(__m256_alignment) static constexpr std::pair<std::array<float, 32>, std::array<float, 32>> c_bandpass_filter = initializeBandpassFilter();
+    static constexpr int c_bandpass_filter_size = c_bandpass_filter.first.size();
+
+    alignas(__m256_alignment) static constexpr std::array<float, 32> c_lowpass_filter = initializeLowpassFilter();
+    static constexpr int c_lowpass_filter_size = c_lowpass_filter.size();
+
+    alignas(__m256_alignment) static constexpr std::array<float, 16> c_rrc_filter = initializeRrcFilter();
+    static constexpr int c_rrc_filter_size = c_rrc_filter.size();
+
+    static constexpr int c_input_buffer_size = c_sample_block_size + c_bandpass_filter_size - 1;
+    static constexpr int c_lowpass_in_buffer_size = c_sample_block_size + c_lowpass_filter_size - 1;
+    static constexpr int c_rrc_in_buffer_size = c_sample_block_size / c_decimation_rate + c_rrc_filter_size - 1;
+    static constexpr int c_output_buffer_size = c_sample_block_size / c_decimation_rate;
+
     bool readFloats(float *out, size_t n);
+
+    template<size_t filter_size> void firFilter(
+                const float *input,   // input signal of length at least output_size + filter_length - 1
+                size_t output_size,   // number of output values to compute
+                const std::array<float, filter_size> &filter,  // reversed filter coefficients, needs to be aligned at 32 byte multiple (c_AVX_floats_per_chunk * sizeof(float))
+                float *output);
 
     Logger &m_log;
     const std::string m_filename;
     int m_input_fd;
     const bool m_input_is_fifo;
-    int16_t *m_tmp_input_buffer;
 };
-
 
 #endif //MUSECPP_RFDEMODULATOR_H
