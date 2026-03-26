482c92556ad5d5e6c845549f0caac266c8e6189d
Staffan Ulfberg <staffan@ulfberg.se>
2026-03-26 22:03:03 +0100

    Do not use find_package(PkgConfig). Also Eighen3 is include only and only requires target_include_directories. Use git version only for application. Fix MergeStaticLibs.cmake

--- ac3rf-decode/cmake/MergeStaticLibs.cmake
+++ ac3rf-decode/cmake/MergeStaticLibs.cmake
@@ -1,25 +1,19 @@
 # MergeStaticLibs.cmake
 # Merges LIB2 into OUTPUT (which already contains LIB1's content).
 # Called at build time via: cmake -DOUTPUT=... -DLIB1=... -DLIB2=... -DAR=... -P MergeStaticLibs.cmake
+#
+# Uses an ar MRI script, which is supported by GNU ar and LLVM ar (used by
+# the Nix clang toolchain on both Linux and macOS).  Apple's BSD ar does not
+# support MRI, but that ar is never CMAKE_AR in a Nix build.
 
-if(APPLE)
-    # macOS: libtool is always available and handles archive merging cleanly
-    execute_process(
-        COMMAND libtool -static -o "${OUTPUT}" "${LIB1}" "${LIB2}"
-        RESULT_VARIABLE result
-        ERROR_VARIABLE err
-    )
-else()
-    # Linux: use ar MRI script (supported by GNU ar and LLVM ar)
-    set(mri "${OUTPUT}.mri")
-    file(WRITE "${mri}" "CREATE ${OUTPUT}\nADDLIB ${LIB1}\nADDLIB ${LIB2}\nSAVE\nEND\n")
-    execute_process(
-        COMMAND sh -c "${AR} -M < ${mri}"
-        RESULT_VARIABLE result
-        ERROR_VARIABLE err
-    )
-    file(REMOVE "${mri}")
-endif()
+set(mri "${OUTPUT}.mri")
+file(WRITE "${mri}" "CREATE ${OUTPUT}\nADDLIB ${LIB1}\nADDLIB ${LIB2}\nSAVE\nEND\n")
+execute_process(
+    COMMAND sh -c "${AR} -M < ${mri}"
+    RESULT_VARIABLE result
+    ERROR_VARIABLE err
+)
+file(REMOVE "${mri}")
 
 if(result)
     message(FATAL_ERROR "Failed to merge static libraries: ${err}")
